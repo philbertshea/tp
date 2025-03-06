4e546c353b6da2dc2178b30333d50bc508a6910f
philbertshea <philbertshea@gmail.com>
2025-03-06 22:02:56 +0800

    Add equals() test to MarkAttendanceCommandTest.java
    Method equals() in MarkAttendanceCommand is not
    tested.
    
    Let's
    * Update MarkAttendanceCommandTest.java to test equals()

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -36,7 +36,8 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
-    public static final int VALID_WEEK_NUMBER = 3;
+    public static final int VALID_WEEK_A = 3;
+    public static final int VALID_WEEK_B = 5;
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
--- src/test/java/seedu/address/logic/commands/MarkAttendanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkAttendanceCommandTest.java
@@ -1,14 +1,17 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_WEEK_NUMBER;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_WEEK_A;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_WEEK_B;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.MarkAttendanceCommand.MESSAGE_ARGUMENTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -19,12 +22,35 @@ public class MarkAttendanceCommandTest {
 
     @Test
     public void execute() {
-        final int someWeekNumber = 3;
-
         assertCommandFailure(
-                new MarkAttendanceCommand(INDEX_FIRST_PERSON, VALID_WEEK_NUMBER),
+                new MarkAttendanceCommand(INDEX_FIRST_PERSON, VALID_WEEK_A),
                 model,
-                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), VALID_WEEK_NUMBER)
+                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), VALID_WEEK_A)
         );
     }
+
+    @Test
+    public void equals() {
+        final MarkAttendanceCommand standardCommand = new MarkAttendanceCommand(INDEX_FIRST_PERSON, VALID_WEEK_A);
+
+        // same values -> return true
+        MarkAttendanceCommand commandWithSameValues = new MarkAttendanceCommand(INDEX_FIRST_PERSON, VALID_WEEK_A);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> return true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> return false
+        assertFalse(standardCommand.equals(false));
+
+        // different types -> return false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> return false
+        assertFalse(standardCommand.equals(new MarkAttendanceCommand(INDEX_SECOND_PERSON, VALID_WEEK_A)));
+
+        // different week -> return false
+        assertFalse(standardCommand.equals(new MarkAttendanceCommand(INDEX_FIRST_PERSON, VALID_WEEK_B)));
+    }
+
 }
