397224d22c611792dc2cd27528bb9bdfb23a08ff
philbertshea <philbertshea@gmail.com>
2025-03-06 21:26:40 +0800

    Update MarkAttendanceCommand with attributes
    MarkAttendanceCommand should accept index and week
    number attributes, but currently does not accept these.
    
    Let's
    * Update MarkAttendanceCommand.java to have index
    and week number attributes
    * Update AddressBookParser.java with temporary placeholder input
    for creating the MarkAttendanceCommand instance

--- src/main/java/seedu/address/logic/commands/MarkAttendanceCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkAttendanceCommand.java
@@ -1,17 +1,66 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
- * Marks the attendance of a person within an existing week.
+ * Marks the attendance of a student within an existing week.
  */
 public class MarkAttendanceCommand extends Command {
 
     public static final String COMMAND_WORD = "att";
 
+    // Note that -u and -mc will NOT be implemented yet.
+    // We will settle the mandatory parameters first.
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Marks the attendance of a student as identified"
+            + " by the index number provided, for a particular week"
+            + " as identified by the week number provided. "
+            + "Existing attendance status will be overwritten by the input.\n"
+            + "Mandatory Parameters: -i INDEX (must be a positive integer "
+            + "that is a valid index)"
+            + " -w WEEK NUMBER (must be a positive integer from 1 to 13)\n"
+            + "Optional Parameters: -u (mark as not attended) "
+            + "-mc (mark as on MC)\n"
+            + "Note that either -u OR -mc OR neither can be provided."
+            + "Example: " + COMMAND_WORD + " -i 1 -w 3\n"
+            + "This marks student of index 1 as attended in tutorial week 3.";
+
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Week: %2$d";
+
+    private final Index index;
+
+    private final int week;
+
+    public MarkAttendanceCommand(Index index, int week) {
+        requireAllNonNull(index, week);
+        this.index = index;
+        this.week = week;
+    }
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("test");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(
+                String.format(MESSAGE_ARGUMENTS, index.getOneBased(), week)
+        );
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof MarkAttendanceCommand)) {
+            return false;
+        }
+
+        MarkAttendanceCommand e = (MarkAttendanceCommand) other;
+        return this.index.equals(e.index) && this.week == e.week;
     }
 
 }
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -8,6 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
@@ -17,6 +18,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.MarkAttendanceCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -76,6 +78,10 @@ public class AddressBookParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
+
+        case MarkAttendanceCommand.COMMAND_WORD:
+            // temporary placeholder input while MarkAttendanceCommandParser is not yet done.
+            return new MarkAttendanceCommand(Index.fromOneBased(1), 1);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
