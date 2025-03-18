60ddecbdb111fa8d53a704a45d795ff5c2f9b152
xuxuezhou <xxz020928@gmail.com>
2025-03-18 22:28:38 +0800

    Resolve conflict

--- src/main/java/seedu/tassist/commons/util/StringUtil.java
+++ src/main/java/seedu/tassist/commons/util/StringUtil.java
@@ -24,7 +24,8 @@ public class StringUtil {
 
         String preppedWord = word.trim();
         checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
-        checkArgument(preppedWord.split("\\s+").length == 1, "Word parameter should be a single word");
+        checkArgument(preppedWord.split("\\s+").length == 1,
+                "Word parameter should be a single word");
 
         String[] wordsInPreppedSentence = sentence.split("\\s+");
 
@@ -44,11 +45,17 @@ public class StringUtil {
 
     /**
      * Returns true if {@code s} represents a non-zero unsigned integer.
+     * e.g. 1, 2, 3, ..., {@code Integer.MAX_VALUE} <br>
+     * Will return false for any other non-null string input
+     * e.g. empty string, "-1", "0", "+1", and " 2 " (untrimmed),
+     * "3 0" (contains whitespace), "1 a" (contains letters)
+     * @throws NullPointerException if {@code s} is null.
      */
     public static boolean isNonZeroUnsignedInteger(String s) {
         requireNonNull(s);
         try {
             int value = Integer.parseInt(s);
+            // "+1" is successfully parsed by Integer#parseInt(String)
             return value > 0 && !s.startsWith("+");
         } catch (NumberFormatException nfe) {
             return false;
@@ -57,11 +64,18 @@ public class StringUtil {
 
     /**
      * Returns true if {@code s} represents a valid week.
+     * e.g. 1, 2, 3, ..., 13 <br>
+     * Will return false for any other non-null string input
+     * e.g. empty string, "-1", "0", "+1", and " 2 " (untrimmed),
+     * "3 0" (contains whitespace), "1 a" (contains letters),
+     * "14" (greater than 13)
+     * @throws NullPointerException if {@code s} is null.
      */
     public static boolean isValidWeek(String s) {
         requireNonNull(s);
         try {
             int value = Integer.parseInt(s);
+            // "+1" is successfully parsed by Integer#parseInt(String)
             return value > 0 && value < 14 && !s.startsWith("+");
         } catch (NumberFormatException nfe) {
             return false;
